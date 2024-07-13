4df98b662129b28bce9deb63c4598286eff077ef
PrimChim <gpritam922@gmail.com>
2024-07-13 20:43:19 +0545

    added features: edit and delete items

--- menu/urls.py
+++ menu/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = 'menu'
 urlpatterns = [
     path('', menu, name='menu'),
-    path('add-items',add_items,name='add-items')
+    path('add-items',add_items,name='add-items'),
+    path('edit-items/<int:id>',edit_menu_item,name='edit-item'),
+    path('delete-items/<int:id>',delete_menu_item,name='delete-item'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -16,3 +16,19 @@ def add_items(request):
         menu.save()
         return render(request, 'add-items.html', {'message': 'Item added successfully'})
     return render(request, 'add-items.html')
+
+def edit_menu_item(request, id):
+    menu = MenuItems.objects.get(id=id)
+    if request.method == "POST":
+        menu.name = request.POST['name']
+        menu.price = request.POST['price']
+        menu.description = request.POST['description']
+        menu.save()
+        return render(request, 'edit-item.html', {'message': 'Item updated successfully'})
+    return render(request, 'edit-item.html', {'item': menu})
+
+def delete_menu_item(request, id):
+    item = MenuItems.objects.get(id=id)
+    item.delete()
+    menu = MenuItems.objects.all()
+    return render(request, 'menu.html', {'message': 'Item deleted successfully', 'menu': menu})
