856cdb4d640444be3025ed60a353e67c92e6f74d
PrimChim <gpritam922@gmail.com>
2024-12-16 21:35:33 +0545

    added: template for create_order
    next: handle data in backend for creating an order

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from menu.models import MenuItems
 
 # Create your views here.
 
 def create_orders(request):
-    return HttpResponse("orders page")
+    items = MenuItems.objects.all()
+    return render(request, 'orders/create_orders.html',{"items": items})
